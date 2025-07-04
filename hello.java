c033c3e07cb5d9a9d370161488027d9acce2bce3
Henik19 <rangoonwalahenik19@gmail.com>
2025-07-04 17:13:15 +0530

    Update6 hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 // File: HelloJava.java
 
-public class hello 
+public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
         System.out.println("Hello, Henik!");
